4b305882cab42d234195c69ef8e6383810f02f7d
YuanKQ <kq_yuan@outlook.com>
2018-05-11 15:46:19 +0800

    归并排序。

--- src/Sort/MergeSort.java
+++ src/Sort/MergeSort.java
@@ -3,12 +3,17 @@
  * Date: 18-4-17 下午11:08
  * Email: kq_yuan@outlook.com
  *
- * Description:
+ * Description:  归并排序
+ *               avg:  O(nlogn)
+ *               best: O(nlogn)
+ *               worst:O(nlogn)
+ *               space:O(n)
+ *               稳定
  *
  ******************************/
 package Sort;
 
-import com.sun.org.apache.regexp.internal.REUtil;
+import java.util.Arrays;
 
 public class MergeSort {
     public void mergeSort(int[] array){
@@ -42,11 +47,54 @@ public class MergeSort {
         return ;
     }
 
+    private static void helpMergeSort(int[] array, int low, int mid, int high) {
+        int[] copy = array.clone();
+        int i = mid;
+        int j = high;
+        int cIndex = high;
+        while (i >= low && j > mid){
+            if (array[i] > array[j])
+                copy[cIndex --] = array[i--];
+            else
+                copy[cIndex --] = array[j--];
+        }
+        while (i >= low){
+            copy[cIndex --] = array[i--];
+        }
+        while (j > mid){
+            copy[cIndex --] = array[j--];
+        }
+
+        for (int s = low; s <= high; s ++)
+            array[s] = copy[s];
+        return ;
+    }
+
+    public static void mergeSortIterative(int[] array){
+        System.out.println("Before: " + Arrays.toString(array));
+        int len = 1;
+        int low = 0, mid = 0, high = 0;
+        while (len < array.length){
+            for(int i = 0; i + len < array.length; i += 2*len){
+                low = i;
+                mid = i + len - 1;
+                high = i + 2*len > array.length? array.length-1: i + 2*len-1;
+                helpMergeSort(array, low, mid, high);
+            }
+            len *= 2;
+        }
+        System.out.println("After: " + Arrays.toString(array));
+        System.out.println("=================");
+    }
+
+
     public static void main(String[] args) {
-        int[] array = {10, 1, 4, 7, 5, 9, 24};
-        new MergeSort().mergeSort(array);
-        for (int a: array)
-            System.out.println(a);
+        int[] arr = {1,2,3,4,5,6,7};
+        mergeSortIterative(arr);
+        int[] arr1 = {91,82,73,64,55,46,37,25,14,3};
+        mergeSortIterative(arr1);
+        int[] arr2 = {61,92,13,4,35,26,77};
+        mergeSortIterative(arr2);
 
     }
 }
